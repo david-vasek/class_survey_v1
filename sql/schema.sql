940be6d70d0efeef96f73fa1054c221c59e12421
David Vasek <david.vasek51@gmail.com>
2021-06-23 09:35:54 -0500

    created sql files

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+-- This is our database configuration 
+
+CREATE TABLE rankings (
+    id serial PRIMARY KEY,
+    ranking_title text NOT NULL
+);
+
+CREATE TABLE topics (
+    id serial PRIMARY KEY,
+    topic_name text NOT NULL,
+    topic_score INTEGER REFERENCES rankings(id)
+);
---
+++ sql/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO rankings 
+    (ranking_title)
+VALUES
+('Awesome'),
+('Great'),
+('Good'),
+('Okay'),
+('Poor'),
+('Unranked');
+INSERT INTO topics
+    (topic_name, topic_score)
+('HTML', 6),
+('CSS', 6),
+('Javascript', 6),
+('PostgreSQL', 6),
+('Node', 6),
+('Express', 6);
