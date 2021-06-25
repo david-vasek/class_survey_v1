7863a0c01305b7e6fd4dda7c810bd253900ee85c
David Vasek <david.vasek51@gmail.com>
2021-06-25 11:42:50 -0500

    added error page routes and handling tools

--- models/ClassSurveyModel.js
+++ models/ClassSurveyModel.js
@@ -22,6 +22,30 @@ class ClassSurveyModel {
             return error;
         }
     }
+
+    static async getAllRankings() {
+        try {
+            const response = await db.any(
+                `SELECT * FROM rankings;`
+            )
+            return response;
+        } catch (error) {
+            console.error('ERROR: ', error);
+            return error;
+        }
+    }
+
+    static async updateRanking(topic, topic_score) {
+        try {
+            const response = await db.result(
+                `UPDATE topics SET topic_score = $1 WHERE topic_name = $2;`, [topic_score, topic]
+            );
+            return response;
+        } catch (error) {
+            console.error("ERROR: ", error);
+            return error;
+        }
+    }
 }
 
 module.exports = ClassSurveyModel;
